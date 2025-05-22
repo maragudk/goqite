b80a80dac1a90f94c6fe6d9253659a24f477c0eb
Markus Wüstenberg <markus@maragu.dk>
2025-05-22 12:54:19 +0200

    Switch to maragu.dev/is
    Just a new namespace.

--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require github.com/mattn/go-sqlite3 v1.14.28
 
 require (
 	github.com/jackc/pgx/v5 v5.7.5
-	github.com/maragudk/is v0.1.0
+	maragu.dev/is v0.3.0
 )
 
 require (
--- jobs/runner_test.go
+++ jobs/runner_test.go
@@ -9,8 +9,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/maragudk/is"
 	_ "github.com/mattn/go-sqlite3"
+	"maragu.dev/is"
 
 	"maragu.dev/goqite"
 	internalsql "maragu.dev/goqite/internal/sql"
